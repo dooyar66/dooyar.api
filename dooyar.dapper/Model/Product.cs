5a766d148b1ff95798aca83cd3b5a37057bda3ff
marsh <marshhu@qq.com>
2019-07-23 18:48:17 +0800

    create project

---
+++ dooyar.dapper/Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace dooyar.dapper.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
